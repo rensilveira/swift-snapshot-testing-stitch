163902b4ad3aa6ea2ec202b0e11eb4f97b15b617
Renan Silveira <95193397+rensilveira@users.noreply.github.com>
2025-08-19 13:11:26 +1000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SnapshotTestingStitch",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v16),
     ],
     products: [
         .library(
